addf347dc035454fc50682e3660093708c3ef462
ChrisLambert03 <98918155+ChrisLambert03@users.noreply.github.com>
2024-12-03 17:37:18 -0500

    completed UI implementatoin
    - click and drag trajectory line persists until shot button clicked
    - power slider affects ball speed and trajectory line now
    - updated golf ball sprite and collision radius
    - "Goal" functionality implemented

--- Assets/scripts/UserInterface.cs
+++ Assets/scripts/UserInterface.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class UserInterface : MonoBehaviour 
+public class UserInterface : MonoBehaviour
 {
     public GameObject NextLevScreen;
     public GameObject GameUI;
     public TextMeshProUGUI ShotCountText;
-    public TMP_InputField TextInput;
+    public TMP_InputField PowerTextBox;
     public Slider Slider;
     public int CurrentShotCount = 0;
+    public float CurrentPower = 2f; // Need a Default power set so the line will show
+    public LineRenderer GolfBallLine;
+    public Ball GolfBall;
+
+
+    private void Start()
+    {
+        GolfBallLine = GameObject.Find("Golf Ball").GetComponent<LineRenderer>();
+        GolfBall = GameObject.Find("Golf Ball").GetComponent<Ball>();
+    }
+
     public void GoToScene(string sceneName)
     {
         Debug.Log($"going to: {sceneName}");
@@ -21,21 +32,40 @@ public class UserInterface : MonoBehaviour
         Application.Quit();
         Debug.Log("Game has been slain(quit game)");
     }
+    [ContextMenu("Activate Level Finish")]
     public void ActivateLevelFinish()
     {
         Debug.Log("ActivateLevelFinish has been called");
         GameUI.SetActive(false);
         NextLevScreen.SetActive(true);
     }
-
+    // gets call onValueChange of power slider
     public void SetTextBox()
-    {
-        TextInput.text = Slider.value.ToString("F2");
+    {   // update the current power to the variable and on screen
+        PowerTextBox.text = Slider.value.ToString("F2");
+        CurrentPower = float.Parse(Slider.value.ToString("F2"));
+        // if the line set to active redraw the line as you change up the power
+        if (GolfBallLine.positionCount == 2)
+        {
+            GolfBall.RedrawLine();
+        }
+
     }
+
     public void IncrementShotCount()
     {
         CurrentShotCount++;
         ShotCountText.text = "Shot Count: " + CurrentShotCount.ToString();
         Debug.Log($"IncrementShotCount called, shot is now{CurrentShotCount}");
+    }
+
+    public float GetPower()
+    {
+        return CurrentPower;
+    }
+
+    public void ShootGolfBall()
+    {
+        if (GolfBall.ShootBall()) { IncrementShotCount(); }
     }
 }
