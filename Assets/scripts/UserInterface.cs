b3923e31a86b8e54d246ec0898b6cc523f3930e3
bradenmaillet <Bradenmaillet@gmail.com>
2024-12-03 12:03:07 -0500

    added Shout Count
    -UI Shot count box with text
    -public TextMeshProUGUI ShotCountText;
    -IncrementShotCount() method within userinterface

--- Assets/scripts/UserInterface.cs
+++ Assets/scripts/UserInterface.cs
@@ -7,8 +7,10 @@ public class UserInterface : MonoBehaviour
 {
     public GameObject NextLevScreen;
     public GameObject GameUI;
+    public TextMeshProUGUI ShotCountText;
     public TMP_InputField TextInput;
     public Slider Slider;
+    public int CurrentShotCount = 0;
     public void GoToScene(string sceneName)
     {
         Debug.Log($"going to: {sceneName}");
@@ -29,5 +31,11 @@ public class UserInterface : MonoBehaviour
     public void SetTextBox()
     {
         TextInput.text = Slider.value.ToString("F2");
+    }
+    public void IncrementShotCount()
+    {
+        CurrentShotCount++;
+        ShotCountText.text = "Shot Count: " + CurrentShotCount.ToString();
+        Debug.Log($"IncrementShotCount called, shot is now{CurrentShotCount}");
     }
 }
